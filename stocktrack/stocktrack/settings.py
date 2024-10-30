e170f210deb81e043d9c9e66f14f08863253c222
Joshp13346 <147950821+Joshp13346@users.noreply.github.com>
2024-10-30 16:47:18 -0500

    Update settings.py

--- stocktrack/stocktrack/settings.py
+++ stocktrack/stocktrack/settings.py
@@ -74,9 +74,13 @@ WSGI_APPLICATION = "stocktrack.wsgi.application"
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'stocktrack499password',
+        'HOST': 'stocktrack-db.cl0cq8a0wy8a.us-east-2.rds.amazonaws.com',
+        'PORT': '5432'
     }
 }
 
