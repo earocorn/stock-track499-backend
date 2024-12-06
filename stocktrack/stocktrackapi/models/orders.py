b3339374a57ed16e03a4e40f99568ad563f541ff
Kalyn Stricklin <kalynstricklin@botts-inc.com>
2024-12-05 19:44:43 -0600

    part number

--- stocktrack/stocktrackapi/models/orders.py
+++ stocktrack/stocktrackapi/models/orders.py
@@ -6,7 +6,7 @@ class PurchaseOrder(models.Model):
     # ID already auto-populates, dont need to specify
     po_number = models.IntegerField()
     part_name = models.TextField(default='Unknown Item', blank=False, null=False)
-    part_number = models.IntegerField(unique=True, blank=False, null=False)
+    part_number = models.IntegerField(blank=False, null=False)
     supplier_id = models.IntegerField()
     qty = models.IntegerField()
     due_date = models.DateField()
